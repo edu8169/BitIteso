59a638b18f6715ecef7b4441d6eb9086aca79bcc
NAVARRO <eduardo.navarro@capgemini.com>
2023-11-02 17:52:11 -0600

    Finished login module

--- Login/core/urls_proyect.py
+++ Login/core/urls_proyect.py
@@ -1,25 +1,10 @@
-"""
-URL configuration for Login project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.urls import path
-from .views import home,transaction_view
+from django.contrib.auth.views import LoginView  # Importa la vista de inicio de sesión de Django
+from .views import home, transaction_view, exit
 
 urlpatterns = [
     path('', home, name='home'),
-    path('transaction_view/',transaction_view, name='transaction_view')
-
+    path('transaction_view/', transaction_view, name='transaction_view'),
+    path('login/', LoginView.as_view(), name='login'),  # Configura la URL de inicio de sesión
+    path('logout/', exit, name='exit')
 ]
--- Login/core/views.py
+++ Login/core/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 # Create your views here.
 def home(request):
     return render(request, 'core/home.html')
 
+@login_required
 def transaction_view(request):
     return render(request, 'core/transaction_view.html')
+
+def exit(request):
+    logout(request)
+    return redirect('home')
